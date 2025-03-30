796245f0873b8e8849c6b525676042e484a404ab
Kev <138612151+KevinDKao@users.noreply.github.com>
2025-03-30 16:07:50 -0500

    Added webapp only

--- app/__init__.py
+++
@@ -1,2 +0,0 @@
-# This file makes the app directory a Python package
-# from app.app import app, server
--- notebooks/drafting_models/feathers/explore.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_feather(
-    r"C:\Users\kkao\Documents\GitHub\OwlAboutFootball_S25\notebooks\kkao_models\feathers\draft_data2020.feather"
-)
-
-df.to_csv("draft_data2020.csv", index=False)
